2ebb47a704fa4ecdcf0f0f2a2ed2c7fe45d62c91
Mateo Villada <villadamateo64@gmail.com>
2025-02-10 17:36:50 -0500

    Update main.c to test insert() and get() functionality

--- main.c
+++ main.c
@@ -2,8 +2,18 @@
 #include "kv_store.h"
 
 int main() {
+    // Crear tabla hash
     HashTable *table = createHashTable();
-    printf("Tabla hash creada con éxito\n");
+    printf("Tabla hash creada\n");
+
+    // Insertar datos
+    insert(table, "steam_id_123", "Counter-Strike");
+    insert(table, "steam_id_456", "Dota 2");
+
+    // Verificar inserción con get()
+    printf("User steam_id_123 plays: %s\n", get(table, "steam_id_123"));
+    printf("User steam_id_456 plays: %s\n", get(table, "steam_id_456"));
+
     freeHashTable(table);
     return 0;
 }
